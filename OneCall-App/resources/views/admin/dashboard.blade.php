85f5e89ae31e534c1f9bc26fb96df81c7c0193dd
Charles <Chaeldemits@gmail.com>
2022-05-29 20:41:56 +0800

    cleaning

--- OneCall-App/resources/views/admin/dashboard.blade.php
+++ OneCall-App/resources/views/admin/dashboard.blade.php
@@ -370,51 +370,13 @@
       <!-- News & Updates Traffic -->
       <div class="card" style="background-color:#4B0082">
         <div class="filter">
-          <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-            <li class="dropdown-header text-start">
-              <h6>Filter</h6>
-            </li>
-
-            <li><a class="dropdown-item" href="#">Today</a></li>
-            <li><a class="dropdown-item" href="#">This Month</a></li>
-            <li><a class="dropdown-item" href="#">This Year</a></li>
-          </ul>
+          <a class="icon" href="#" data-bs-toggle="dropdown"></a>
         </div>
 
         <div class="card-body pb-0">
           <h5 class="card-title" style="color:#fff">News &amp; Updates <span style="color:#fff">| Today</span></h5>
 
           <div class="news">
-            <div class="post-item clearfix">
-              <img src="assets/img/news-1.jpg" alt="">
-              <h4><a href="#" style="color:#fff">Nihil blanditiis at in nihil autem</a></h4>
-              <p>Sit recusandae non aspernatur laboriosam. Quia enim eligendi sed ut harum...</p>
-            </div>
-
-            <div class="post-item clearfix">
-              <img src="assets/img/news-2.jpg" alt="">
-              <h4><a href="#" style="color:#fff">Quidem autem et impedit</a></h4>
-              <p>Illo nemo neque maiores vitae officiis cum eum turos elan dries werona nande...</p>
-            </div>
-
-            <div class="post-item clearfix">
-              <img src="assets/img/news-3.jpg" alt="">
-              <h4><a href="#" style="color:#fff">Id quia et et ut maxime similique occaecati ut</a></h4>
-              <p>Fugiat voluptas vero eaque accusantium eos. Consequuntur sed ipsam et totam...</p>
-            </div>
-
-            <div class="post-item clearfix">
-              <img src="assets/img/news-4.jpg" alt="">
-              <h4><a href="#" style="color:#fff">Laborum corporis quo dara net para</a></h4>
-              <p>Qui enim quia optio. Eligendi aut asperiores enim repellendusvel rerum cuder...</p>
-            </div>
-
-            <div class="post-item clearfix">
-              <img src="assets/img/news-5.jpg" alt="">
-              <h4><a href="#" style="color:#fff">Et dolores corrupti quae illo quod dolor</a></h4>
-              <p>Odit ut eveniet modi reiciendis. Atque cupiditate libero beatae dignissimos eius...</p>
-            </div>
 
           </div><!-- End sidebar recent posts-->
 
--- OneCall-App/resources/views/admin/footer.blade.php
+++ OneCall-App/resources/views/admin/footer.blade.php
@@ -1,13 +1,5 @@
-  <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer" style="background-color:#aeb3ef">
+<footer id="footer" class="footer" style="background-color:#aeb3ef">
     <div class="copyright">
-      &copy; Copyright <strong><span>OneCallAwayEmergencyApp</span></strong>. All Rights Reserved
+      &copy; <strong><span>OneCallAwayEmergencyApp</span></strong>
     </div>
-    <div class="credits">
-      <!-- All the links in the footer should remain intact. -->
-      <!-- You can delete the links only if you purchased the pro version. -->
-      <!-- Licensing information: https://bootstrapmade.com/license/ -->
-      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      
-    </div>
-  </footer><!-- End Footer -->
+  </footer>

d60fc139bd66ceb91e4ae8b93dbc7cecbb1d3171
Kathleen Fernandez <78854155+KathleenFernandez@users.noreply.github.com>
2022-06-20 22:28:51 +0800

    update

--- OneCall-App/resources/views/admin/dashboard.blade.php
+++ OneCall-App/resources/views/admin/dashboard.blade.php
@@ -25,8 +25,8 @@
         <div class="col-xxl-4 col-md-6">
           <div class="card info-card sales-card" style="background-color:#6266f5">
             <div class="card-body">
-              <h5 class="card-title" style="color:#fff">Police Department <span style="color:#fff">|
-               <span class="badge bg-primary badge-number"style="color:#fff" >4</span> </span></h5>
+              <h5 class="card-title" style="color:#fff">Police Department <span style="color:#fff">
+               <span class="badge bg-primary badge-number"style="color:#fff" ></span> </span></h5>
 
               <div class="d-flex align-items-center">
                 <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
@@ -48,8 +48,8 @@
           <div class="card info-card revenue-card" style="background-color:#6266f5">
 
             <div class="card-body">
-              <h5 class="card-title" style="color:#fff">Fire Department <span style="color:#fff">| 
-              <span class="badge bg-primary badge-number"style="color:#fff" >4</span> </span></h5>
+              <h5 class="card-title" style="color:#fff">Fire Department <span style="color:#fff">
+              <span class="badge bg-primary badge-number"style="color:#fff" ></span> </span></h5>
               <div class="d-flex align-items-center">
                 <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                   <i class="bi bi-droplet"></i>
@@ -69,8 +69,8 @@
 
           <div class="card info-card customers-card" style="background-color:#6266f5">
             <div class="card-body">
-              <h5 class="card-title" style="color:#fff">Medical Department<span style="color:#fff">|
-              <span class="badge bg-primary badge-number"style="color:#fff" >4</span> </span></h5>
+              <h5 class="card-title" style="color:#fff">Medical Department<span style="color:#fff">
+              <span class="badge bg-primary badge-number"style="color:#fff" ></span> </span></h5>
               <div class="d-flex align-items-center">
                 <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                   <i class="bi bi-activity"></i>
@@ -86,87 +86,6 @@
           </div>
 
         </div>
-
-        <!-- <div class="col-12">
-          <div class="card" style="background-color:#343965">
-
-            <div class="filter">
-              <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-              <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li class="dropdown-header text-start">
-                  <h6>Filter</h6>
-                </li>
-
-                <li><a class="dropdown-item" href="#">Today</a></li>
-                <li><a class="dropdown-item" href="#">This Month</a></li>
-                <li><a class="dropdown-item" href="#">This Year</a></li>
-              </ul>
-            </div>
-
-            <div class="card-body">
-              <h5 class="card-title" style="color:#fff">Reports <span style="color:#fff">/Today</span></h5>
-
-
-              <div id="reportsChart"></div>
-
-              <script>
-                document.addEventListener("DOMContentLoaded", () => {
-                  new ApexCharts(document.querySelector("#reportsChart"), {
-                    series: [{
-                      name: 'Total Reports', 
-                      data: [31, 40, 28, 51, 42, 82, 56],
-                    }, {
-                      name: 'Total Downloads',
-                      data: [11, 32, 45, 32, 34, 52, 41]
-                    }, {
-                      name: 'Total Users',
-                      data: [15, 11, 32, 18, 9, 24, 11]
-                    }],
-                    chart: {
-                      height: 350,
-                      type: 'area',
-                      toolbar: {
-                        show: false
-                      },
-                    },
-                    markers: {
-                      size: 4
-                    },
-                    colors: ['#C71585', '#32CD32', '#ff771d'], 
-                    fill: {
-                      type: "gradient",
-                      gradient: {
-                        shadeIntensity: 1,
-                        opacityFrom: 0.3,
-                        opacityTo: 0.4,
-                        stops: [0, 90, 100]
-                      }
-                    },
-                    dataLabels: {
-                      enabled: false
-                    },
-                    stroke: {
-                      curve: 'smooth',
-                      width: 2
-                    },
-                    xaxis: {
-                      type: 'datetime',
-                      categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-                    },
-                    tooltip: {
-                      x: {
-                        format: 'dd/MM/yy HH:mm'
-                      },
-                    }
-                  }).render();
-                });
-              </script>
-
-
-            </div>
-
-          </div>
-        </div> -->
 
         <div class="col-12">
           <div class="card recent-sales overflow-auto" style="background-color:#343965">
@@ -203,7 +122,7 @@
                     <td style="color:#fff">Samantha123@gmail.com</td>
                     <td><a href="#" style="color:#fff">Fire Emergency</a></td>
                     <td style="color:#fff">Kauswagan Cagayan De Oro City</td>
-                    <td><span class="badge bg-success">Approved</span></td>
+                    <td><span class="badge bg-success">Responded</span></td>
                   </tr>
                 </tbody>
               </table>
